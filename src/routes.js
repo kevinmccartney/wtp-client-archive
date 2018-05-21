0458fb69e874a2762e3dae3f57115656c8bbdf58
Kevin McCartney <hello@kevinmccartney.is>
2018-05-21 15:10:14 -0400

    Trying to fix route config

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,26 @@
 import Splash from '@views/Splash';
 import NotFound from '@views/NotFound';
 import Party from '@views/Party';
+import Root from '@views/Root';
 
-const routes = [
-  {
+const routes = [{
+  component: Root,
+  routes: [{
     path: '/',
     exact: true,
     component: Splash,
-  },
-  {
-    path: '/party',
-    exact: true,
-    component: Party,
-  },
-  {
-    path: '*',
-    component: NotFound,
-  },
-];
+    routes: [
+      {
+        path: '/party',
+        exact: true,
+        component: Party,
+      },
+      {
+        path: '/*',
+        component: NotFound,
+      },
+    ],
+  }],
+}];
 
 export default routes;
