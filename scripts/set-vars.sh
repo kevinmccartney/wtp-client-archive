e440209d5852220b4d8cea3483d49e0febd9ed5d
Kevin McCartney <hello@kevinmccartney.is>
2019-09-06 11:16:27 -0400

    More bash script fixes

--- scripts/set-vars.sh
+++ scripts/set-vars.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if[$CIRCLE_BRANCH == 'master']; then
+if [$CIRCLE_BRANCH == 'master']; then
   S3_DEST = $S3_DEST_MASTER;
   NG_BUILD_ARGS = $NG_BUILD_ARGS_MASTER;
-else if[$CIRCLE_BRANCH == 'qa']; then
+else if [$CIRCLE_BRANCH == 'qa']; then
   S3_DEST = $S3_DEST_QA;
   NG_BUILD_ARGS = $NG_BUILD_ARGS_QA;
-else if[$CIRCLE_BRANCH == 'develop']; then
+else if [$CIRCLE_BRANCH == 'develop']; then
   S3_DEST = $S3_DEST_DEV;
   NG_BUILD_ARGS = $NG_BUILD_ARGS_DEV;
 fi
