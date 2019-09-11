7b821e07137bd168e2ff68f22ed7d05be798769e
Kevin McCartney <hello@kevinmccartney.is>
2019-09-11 00:08:51 -0400

    Upating shell scripts

--- scripts/set-vars.sh
+++ scripts/set-vars.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-if [ "$CIRCLE_BRANCH" == 'master' ]
+if [ $CIRCLE_BRANCH == 'master' ]
   then
     export S3_DEST="$S3_DEST_MASTER";
     export NG_BUILD_ARGS=$NG_BUILD_ARGS_MASTER;
-elif [ "$CIRCLE_BRANCH" == 'qa' ]
+elif [ $CIRCLE_BRANCH == 'qa' ]
   then
-  export S3_DEST="$S3_DEST_QA";
+  export S3_DEST=$S3_DEST_QA;
   export NG_BUILD_ARGS=$NG_BUILD_ARGS_QA;
-elif [ "$CIRCLE_BRANCH" == 'develop' ]
+elif [ $CIRCLE_BRANCH == 'develop' ]
   then
   export S3_DEST="$S3_DEST_DEV";
   export NG_BUILD_ARGS=$NG_BUILD_ARGS_DEV;
